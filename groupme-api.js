53f9b35581206dff6f918a882a20f38bf78cc2d0
Brant Goings <bgraulph@gmail.com>
2022-08-29 10:32:32 -0400

    /list

--- groupme-api.js
+++ groupme-api.js
@@ -161,10 +161,10 @@ const filterAndPostWeeklyList = async (msgList) => {
     console.log(msgList.length)
 
     for (let i = 0; i < msgList.length; i++) {
-      console.log(msgList[i].created_at)
+      console.log(msgList[i].created_at * 1000)
       // console.log(String(Date.parse(msgList[i].created_at)))
-      console.log(msgList[i].created_at * 1000 > roundedDate)
-      console.log(Date.parse(msgList[i].created_at * 1000) > roundedDate)
+      console.log(new Date(msgList[i].created_at * 1000) > Date.parse(roundedDate))
+      console.log(Date.parse(msgList[i].created_at * 1000) > Date.parse(roundedDate))
       // console.log(msgList[i].created_at.toLocaleDateString() > roundedDate)
     }
 
